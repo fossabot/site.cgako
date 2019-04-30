c1c122130c81b7d94c9a047d3de7fded01d625b2
Goomba41 <anton.borodawkin@yandex.ru>
2019-04-30 12:31:09 +0000

    API пользователей, доделано обновление, вывод трейсбэка если в запросе параметр dbg, после добавления в ответе ссылка на новый объект. Сделать ссылку на объект при обновлении, пагинацию списка.

--- app/API/v0/views.py
+++ app/API/v0/views.py
@@ -1,32 +1,46 @@
 #! env/bin/python3.6
 # -*- coding: utf8 -*-
+import sys, traceback
 
 from app import app, db
-from flask import json, jsonify, Blueprint, request, Response
+from flask import json, jsonify, Blueprint, request, Response, url_for
 
 from app.models import cmsUsers
 from app.models import cmsUsersSchema
 
 API0 = Blueprint('API0', __name__)
 
-#Пользователи
+# Функции
+
+# Пагинация
+
+def pagination_of_list(query_result, url, start, limit):
+    
+    return "OK"
+
+# Пользователи
 @API0.route('/users', methods=['GET'])
 def get_users():
-    
     try:
         user_schema = cmsUsersSchema(many=True)
-        user = cmsUsers.query.all()
-        udata = user_schema.dump(user)
+        users = cmsUsers.query.all()
+        udata = user_schema.dump(users)
 
         response = Response(
-            response=json.dumps(udata),
+            response=json.dumps(udata.data),
             status=200,
             mimetype='application/json'
         )
         
     except:
+        debug = request.args.get("dbg")
+        if debug is not None:
+            text = traceback.format_exc()
+        else:
+            text = "Серверная ошибка!"
+            
         response = Response(
-            response=json.dumps({'type':'error', 'text':'Серверная ошибка!'}),
+            response=json.dumps({'type':'error', 'text':text}),
             status=500,
             mimetype='application/json'
         )
@@ -34,7 +48,7 @@ def get_users():
     return response
     
 @API0.route('/users/<int:uid>', methods=['GET'])
-def get_users_by_id(uid):
+def get_user_by_id(uid):
 
     try:
         user_schema = cmsUsersSchema()
@@ -42,14 +56,20 @@ def get_users_by_id(uid):
         udata = user_schema.dump(user)
 
         response = Response(
-            response=json.dumps(udata),
+            response=json.dumps(udata.data),
             status=200,
             mimetype='application/json'
         )
         
     except:
+        debug = request.args.get("dbg")
+        if debug is not None:
+            text = traceback.format_exc()
+        else:
+            text = "Серверная ошибка!"
+            
         response = Response(
-            response=json.dumps({'type':'error', 'text':'Серверная ошибка!'}),
+            response=json.dumps({'type':'error', 'text':text}),
             status=500,
             mimetype='application/json'
         )
@@ -58,9 +78,8 @@ def get_users_by_id(uid):
 
 @API0.route('/users', methods=['POST'])
 def post_users():
-    
+
     try:
-        
         post_data = request.get_json()
         
         check = cmsUsers.query.filter((cmsUsers.login==post_data['login'])|(cmsUsers.email==post_data['email'])|(cmsUsers.phone==post_data["phone"])).first()
@@ -72,7 +91,7 @@ def post_users():
                 mimetype='application/json'
             )
         else:
-            users = cmsUsers(
+            user = cmsUsers(
                 login = post_data['login'],
                 password = post_data['password'],
                 name = post_data['name'],
@@ -84,32 +103,68 @@ def post_users():
                 status = post_data["status"]
             )
             
-            db.session.add(users)
+            db.session.add(user)
             db.session.commit()
-            
+
             response = Response(
-                response=json.dumps({'type':'success', 'text':'Успешно добавлено!'}),
+                response=json.dumps({'type':'success', 'text':'Успешно добавлено!', 'link':url_for('.get_user_by_id', uid=user.id)}),
                 status=200,
                 mimetype='application/json'
             )
         
     except:
+        debug = request.args.get("dbg")
+        if debug is not None:
+            text = traceback.format_exc()
+        else:
+            text = "Серверная ошибка!"
+            
         response = Response(
-            response=json.dumps({'type':'error', 'text':'Серверная ошибка!'}),
+            response=json.dumps({'type':'error', 'text':text}),
             status=500,
             mimetype='application/json'
         )
     
     return response
     
-# @API0.route('/users', methods=['UPDATE'])
-# def update_users():
+@API0.route('/users/<int:uid>', methods=['PUT'])
+def update_users(uid):
     
-    # user_schema = cmsUsersSchema(many=True)
-    # user = cmsUsers.query.all()
-    # udata = user_schema.dump(user)
+    try:
+        update_data = request.get_json()
 
-    # return jsonify(udata.data)
+        check = cmsUsers.query.filter((cmsUsers.login==update_data['login'])|(cmsUsers.email==update_data['email'])|(cmsUsers.phone==update_data["phone"])).first()
+        
+        if check.id != uid:
+            response = Response(
+                response=json.dumps({'type':'error', 'text':'Пользователь с такими данными существует!'}),
+                status=422,
+                mimetype='application/json'
+            )
+        else:
+            user = cmsUsers.query.filter_by(id=uid).update(update_data)
+            db.session.commit()
+
+            response = Response(
+                response=json.dumps({'type':'success', 'text':'Успешно обновлен пользователь с id='+str(uid)+'!', 'link':url_for('.get_user_by_id', uid=uid)}),
+                status=200,
+                mimetype='application/json'
+            )
+        
+    except:
+        debug = request.args.get("dbg")
+        if debug is not None:
+            text = traceback.format_exc()
+        else:
+            text = "Серверная ошибка!"
+            
+        response = Response(
+            response=json.dumps({'type':'error', 'text':text}),
+            status=500,
+            mimetype='application/json'
+        )
+    
+    return response
     
 @API0.route('/users/<int:uid>', methods=['DELETE'])
 def delete_users(uid):
@@ -127,8 +182,14 @@ def delete_users(uid):
         )
         
     except:
+        debug = request.args.get("dbg")
+        if debug is not None:
+            text = traceback.format_exc()
+        else:
+            text = "Серверная ошибка!"
+            
         response = Response(
-            response=json.dumps({'type':'error', 'text':'Серверная ошибка!'}),
+            response=json.dumps({'type':'error', 'text':text}),
             status=500,
             mimetype='application/json'
         )
