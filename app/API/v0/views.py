ec60f0490c98495d792deb1f2d40a699c7941451
Goomba41 <anton.borodawkin@yandex.ru>
2019-05-14 07:33:39 +0000

    Установлен линтер. Прогоняем код на соответствие PEP

--- app/API/v0/views.py
+++ app/API/v0/views.py
@@ -1,12 +1,14 @@
 #! env/bin/python3.6
 # -*- coding: utf8 -*-
-import sys, traceback
 
-from app import app, db
-from flask import json, jsonify, Blueprint, request, Response, url_for, abort
+"""Представления API версии 0. Содержит следующие представления: Пользователи, Новости"""
 
-from app.models import cmsUsers
-from app.models import cmsUsersSchema
+import traceback
+
+from flask import json, Blueprint, request, Response, url_for, abort
+
+from app import db
+from app.models import cmsUsers, cmsUsersSchema
 
 from config import LIMIT
 
@@ -16,49 +18,58 @@ API0 = Blueprint('API0', __name__)
 # Функции
 # ------------------------------------------------------------
 
-# Вывод серверной ошибки с трейсом
 def server_error(dbg=None):
+    """Вывод серверной ошибки с трейсом. Параметр dbg отвечает за вывод в формате traceback."""
+
     if dbg is not None:
         text = traceback.format_exc()
     else:
         text = "Серверная ошибка!"
-    
+
     response = Response(
         response=json.dumps({'type':'error', 'text':text}),
         status=500,
         mimetype='application/json'
     )
-    
+
     return response
 
 # Пагинация
 def pagination_of_list(query_result, url, start, limit):
-    
+    """ Пагинация результатов запроса. Принимает параметры: результат запроса (json),
+    URL API для генерации ссылок, стартовая позиция, количество выводимых записей"""
+
     records_count = len(query_result)
-    
-    if (records_count < start): # Проверка существования страницы
-        abort(404)                       # Сделать адекватную обработку
+
+    if records_count < start:       # Проверка существования страницы
+        abort(404)                  # Сделать адекватную обработку
 
     response_obj = {}
     response_obj['start'] = start
     response_obj['limit'] = limit
     response_obj['count'] = records_count
-    
+
     # Создаем URL на предыдущую страницу
     if start == 1:
         response_obj['previous'] = ''
     else:
         start_copy = max(1, start - limit) # Странный просчет последней страницы
         limit_copy = start - 1
-        response_obj['previous'] = url_for('API0.get_users', start=start_copy, limit=limit_copy, _external=True)
-        
+        response_obj['previous'] = url_for('API0.get_users',
+                                           start=start_copy,
+                                           limit=limit_copy,
+                                           _external=True)
+
     # Создаем URL на следующую страницу
     if start + limit > records_count:
         response_obj['next'] = ''
     else:
         start_copy = start + limit
-        response_obj['next'] = url_for('API0.get_users', start=start_copy, limit=limit, _external=True)
-        
+        response_obj['next'] = url_for('API0.get_users',
+                                       start=start_copy,
+                                       limit=limit,
+                                       _external=True)
+
     # Отсеивание результатов запроса
     response_obj['results'] = query_result[(start - 1):(start - 1 + limit)]
 
@@ -68,39 +79,43 @@ def pagination_of_list(query_result, url, start, limit):
 # Пользователи
 # ------------------------------------------------------------
 
-# Получение полного списка
 @API0.route('/users', methods=['GET'])
 def get_users():
-    
+    """ Получение полного списка пользователей в json"""
+
     try:
-        
+
         all_records = request.args.get("all")
-        
+
         user_schema = cmsUsersSchema(many=True)
         users = cmsUsers.query.all()
         udata = user_schema.dump(users)
         udata = udata.data
-        
+
         if all_records is None:
-            udata = pagination_of_list(udata, '/API/v0/users', start=int(request.args.get('start', 1)), limit=int(request.args.get('limit', LIMIT)))
+            udata = pagination_of_list(udata,
+                                       '/API/v0/users',
+                                       start=int(request.args.get('start', 1)),
+                                       limit=int(request.args.get('limit', LIMIT)))
 
         response = Response(
             response=json.dumps(udata),
             status=200,
             mimetype='application/json'
         )
-        
-    except:
-        
+
+    except Exception:
+
         response = server_error(request.args.get("dbg"))
-        
+
     return response
-    
-# Получение одного по номеру
+
 @API0.route('/users/<int:uid>', methods=['GET'])
 def get_user_by_id(uid):
+    """ Получение одного пользователя по id в json"""
 
     try:
+
         user_schema = cmsUsersSchema()
         user = cmsUsers.query.get(uid)
         udata = user_schema.dump(user)
@@ -110,103 +125,124 @@ def get_user_by_id(uid):
             status=200,
             mimetype='application/json'
         )
-        
-    except:
+
+    except Exception:
+
         response = server_error(request.args.get("dbg"))
-    
+
     return response
 
-# Вставка в БД
 @API0.route('/users', methods=['POST'])
 def post_users():
+    """ Добавление записи пользователя в БД"""
 
     try:
+
         post_data = request.get_json()
-        
-        check = cmsUsers.query.filter((cmsUsers.login==post_data['login'])|(cmsUsers.email==post_data['email'])|(cmsUsers.phone==post_data["phone"])).first()
-        
+
+        check = cmsUsers.query.filter(
+            (cmsUsers.login == post_data['login'])|
+            (cmsUsers.email == post_data['email'])|
+            (cmsUsers.phone == post_data["phone"])).first()
+
         if check:
             response = Response(
-                response=json.dumps({'type':'error', 'text':'Пользователь с такими данными существует!'}),
+                response=json.dumps({'type':'error',
+                                     'text':'Пользователь с такими данными существует!'}),
                 status=422,
                 mimetype='application/json'
             )
         else:
             user = cmsUsers(
-                login = post_data['login'],
-                password = post_data['password'],
-                name = post_data['name'],
-                surname = post_data['surname'],
-                patronymic = post_data['patronymic'],
-                birth_date = post_data['birth_date'],
-                email = post_data['email'],
-                phone = post_data["phone"],
-                status = post_data["status"]
+                login=post_data['login'],
+                password=post_data['password'],
+                name=post_data['name'],
+                surname=post_data['surname'],
+                patronymic=post_data['patronymic'],
+                birth_date=post_data['birth_date'],
+                email=post_data['email'],
+                phone=post_data["phone"],
+                status=post_data["status"]
             )
-            
+
             db.session.add(user)
             db.session.commit()
 
             response = Response(
-                response=json.dumps({'type':'success', 'text':'Успешно добавлен пользователь с id='+str(user.id)+'!', 'link':url_for('.get_user_by_id', uid=user.id, _external=True)}),
+                response=json.dumps({'type':'success',
+                                     'text':'Успешно добавлен пользователь с id='+str(user.id)+'!',
+                                     'link':url_for('.get_user_by_id',
+                                                    uid=user.id,
+                                                    _external=True)}),
                 status=200,
                 mimetype='application/json'
             )
-        
-    except:
+
+    except Exception:
+
         response = server_error(request.args.get("dbg"))
-    
+
     return response
 
-# Изменение в БД
 @API0.route('/users/<int:uid>', methods=['PUT'])
 def update_users(uid):
-    
+    """ Изменение записи пользователя в БД"""
+
     try:
+
         update_data = request.get_json()
 
-        check = cmsUsers.query.filter((cmsUsers.login==update_data['login'])|(cmsUsers.email==update_data['email'])|(cmsUsers.phone==update_data["phone"])).first()
-        
+        check = cmsUsers.query.filter(
+            (cmsUsers.login == update_data['login'])|
+            (cmsUsers.email == update_data['email'])|
+            (cmsUsers.phone == update_data["phone"])).first()
+
         if check.id != uid:
             response = Response(
-                response=json.dumps({'type':'error', 'text':'Пользователь с такими данными существует!'}),
+                response=json.dumps({'type':'error',
+                                     'text':'Пользователь с такими данными существует!'}),
                 status=422,
                 mimetype='application/json'
             )
         else:
-            user = cmsUsers.query.filter_by(id=uid).update(update_data)
+            cmsUsers.query.filter_by(id=uid).update(update_data)
             db.session.commit()
 
             response = Response(
-                response=json.dumps({'type':'success', 'text':'Успешно обновлен пользователь с id='+str(uid)+'!', 'link':url_for('.get_user_by_id', uid=uid, _external=True)}),
+                response=json.dumps({'type':'success',
+                                     'text':'Успешно обновлен пользователь с id='+str(uid)+'!',
+                                     'link':url_for('.get_user_by_id', uid=uid, _external=True)}),
                 status=200,
                 mimetype='application/json'
             )
-        
-    except:
+
+    except Exception:
+
         response = server_error(request.args.get("dbg"))
-    
+
     return response
 
-# Удаление из БД
 @API0.route('/users/<int:uid>', methods=['DELETE'])
 def delete_users(uid):
+    """ Удаление записи пользователя из БД"""
 
     try:
+
         user = cmsUsers.query.get(uid)
-            
+
         db.session.delete(user)
         db.session.commit()
-        
+
         response = Response(
             response=json.dumps({'type':'success', 'text':'Успешно удалено!'}),
             status=200,
             mimetype='application/json'
         )
-        
-    except:
+
+    except Exception:
+
         response = server_error(request.args.get("dbg"))
-    
+
     return response
 
 # ------------------------------------------------------------
--- run.py
+++ run.py
@@ -3,5 +3,5 @@
 
 from app import app
 
-if __name__=="__main__":
+if __name__ == "__main__":
     app.run(host="0.0.0.0")
